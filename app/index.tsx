392ad5d2e54b9f5d60ca92b667d16b1e6b1421b1
Christian Sturm <c.sturm@avm.de>
2024-08-26 16:15:53 +0200

    Feat: Add preliminary First App Start Screen to index of of top Stack Navigator

---
+++ app/index.tsx
@@ -0,0 +1,29 @@
+import { Text, View, Button } from "react-native";
+import { TestStyles } from "./(tabs)";
+import { Image } from "expo-image";
+
+const LOGO_SVG = require("@/assets/images/Logo/LogoWithName.svg");
+
+export default function ChooseRegistrationOrLoginScreen() {
+  return (
+    <View
+      style={[
+        {
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        TestStyles,
+      ]}
+    >
+      <Image
+        source={LOGO_SVG}
+        contentFit="contain"
+        style={{ width: "72%", height: "39%" }}
+      />
+      <Button title="Register" onPress={function () {}} />
+      <Button title="Login" onPress={function () {}} />
+      <Button title="Login with Google" onPress={function () {}} />
+    </View>
+  );
+}
