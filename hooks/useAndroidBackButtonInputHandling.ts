6fe99cb5c96597e7d1e098e3777cc6a7a56a8b36
Christian Sturm <c.sturm@avm.de>
2024-09-07 18:31:50 +0200

    Feat: Add custom Hook for handling Android Back Button

---
+++ hooks/useAndroidBackButtonInputHandling.ts
@@ -0,0 +1,19 @@
+import { useEffect, type ComponentProps } from "react";
+import { Keyboard } from "react-native";
+
+/**
+ * An open Input on Android can get closed via the Backbutton or Swipe.
+ *
+ * This hook handles this by clearing all focus, so {@link ComponentProps<TextInput>.onEndEditing()} can fire properly
+ */
+export default function useAndroidBackButtonInputHandling(): void {
+  useEffect(() => {
+    const KEYBOARD_CLOSE = Keyboard.addListener("keyboardDidHide", () => {
+      Keyboard.dismiss();
+    });
+
+    return () => {
+      KEYBOARD_CLOSE.remove();
+    };
+  }, []);
+}
