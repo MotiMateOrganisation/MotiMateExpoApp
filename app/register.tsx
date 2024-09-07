994f6037cd75a82ade3426fc47228576ec4f5a43
Christian Sturm <c.sturm@avm.de>
2024-09-07 18:33:23 +0200

    Feat: Implement Validation Logic

--- app/register.tsx
+++ app/register.tsx
@@ -1,4 +1,11 @@
-import { View, Button, TextInput, Text } from "react-native";
+import {
+  View,
+  Button,
+  TextInput,
+  Text,
+  NativeSyntheticEvent,
+  TextInputEndEditingEventData,
+} from "react-native";
 import { TestStyles } from "./(tabs)";
 import useRegistrationState, {
   NetworkFailure,
@@ -6,9 +13,18 @@ import useRegistrationState, {
   RegistrationLoading,
 } from "@/hooks/useRegistrationState";
 import { RegistrationDetails } from "@/data/repository/UserRepository";
+import { useState } from "react";
+import useRegistrationValidityState, {
+  type NullBoolean,
+} from "@/hooks/useRegistrationValidityState";
+import useAndroidBackButtonInputHandling from "@/hooks/useAndroidBackButtonInputHandling";
 
 export default function RegistrationScreen() {
+  useAndroidBackButtonInputHandling();
+
   let [registrationState, startRegistration] = useRegistrationState();
+  let [inputValidity, updateInputValidity] = useRegistrationValidityState();
+  useState<NullBoolean>(null);
 
   return (
     <View
@@ -23,22 +39,16 @@ export default function RegistrationScreen() {
     >
       {/* TODO: Wrap Label in Pressable and focus on useRef of Pressable */}
       <Text>Register</Text>
-      <View>
-        <Text>Username (Nickname)</Text>
-        <TextInput style={TestStyles}></TextInput>
-      </View>
-      <View>
-        <Text>Email</Text>
-        <TextInput style={TestStyles}></TextInput>
-      </View>
-      <View>
-        <Text>Password</Text>
-        <TextInput style={TestStyles}></TextInput>
-      </View>
-      <View>
-        <Text>Confirm Password</Text>
-        <TextInput style={TestStyles}></TextInput>
-      </View>
+
+      <InputComponent
+        labelText="Username (Nickname)"
+        isValid={inputValidity.usernameValidity ?? true}
+        hint=""
+        onEndEditing={function ({ nativeEvent: { text } }) {
+          const REGEX = /^[\w\-\.]{1,63}@[\w-]{1,63}\.[\w]{2,63}$/;
+          updateInputValidity({ usernameValidity: REGEX.test(text) });
+        }}
+      />
 
       <Button
         title={
@@ -50,12 +60,45 @@ export default function RegistrationScreen() {
           // TODO: Pass Input States
           startRegistration(new RegistrationDetails("", "", ""));
         }}
-        disabled={registrationState instanceof RegistrationLoading}
+        disabled={
+          inputValidity.areAnyInputsInvalid() ||
+          registrationState instanceof RegistrationLoading
+        }
       />
       {registrationState instanceof RegistrationFailure ||
       registrationState instanceof NetworkFailure ? (
         <Text>{registrationState.message}</Text>
       ) : null}
+    </View>
+  );
+}
+
+const InvalidStyles = { borderColor: "red", borderWidth: 2 };
+
+function InputComponent({
+  labelText,
+  isValid,
+  hint,
+  onEndEditing,
+  isSecureText = false,
+}: {
+  labelText: string;
+  isValid: boolean;
+  hint: string;
+  onEndEditing: (
+    event: NativeSyntheticEvent<TextInputEndEditingEventData>,
+  ) => void;
+  isSecureText?: boolean;
+}) {
+  return (
+    <View>
+      <Text>{labelText}</Text>
+      <TextInput
+        secureTextEntry={isSecureText}
+        style={[TestStyles, isValid ? {} : InvalidStyles]}
+        onEndEditing={onEndEditing}
+      />
+      <Text style={isValid ? {} : {}}>{hint}</Text>
     </View>
   );
 }
