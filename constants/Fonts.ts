163c5a35d3c38c0c5b8c5cb51b8fd93e307788c4
Christian Sturm <c.sturm@avm.de>
2024-09-08 18:35:21 +0200

    Feat: Add Fontstyles

---
+++ constants/Fonts.ts
@@ -0,0 +1,13 @@
+import { StyleProp, TextStyle } from "react-native";
+
+const h5: StyleProp<TextStyle> = {
+  fontSize: 28,
+  fontWeight: "bold",
+  lineHeight: 28,
+};
+
+export const Fonts = {
+  title: {
+    h5,
+  },
+};
