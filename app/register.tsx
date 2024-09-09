3b59334412589e314278bf298820f9abf4e2204d
Christian Sturm <c.sturm@avm.de>
2024-09-09 18:05:01 +0200

    Feat: Implement Auto Focus Next and first Eye Icon

--- app/register.tsx
+++ app/register.tsx
@@ -14,85 +14,136 @@ import useRegistrationState, {
   RegistrationLoading,
 } from "@/hooks/useRegistrationState";
 import useRegistrationValidityState from "@/hooks/useRegistrationValidityState";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Text, View } from "react-native";
-import { TestStyles } from "./(tabs)";
+import { TextInput } from "react-native-gesture-handler";
+import { Fonts } from "@/constants/Fonts";
+import { Colors } from "@/constants/Colors";
 
 export default function RegistrationScreen() {
   useAndroidBackButtonInputHandling();
 
+  let [isBeingEdited, setIsBeingEdited] = useState(true);
   let [registrationState, startRegistration] = useRegistrationState();
   let [inputValidity, updateInputValidity] = useRegistrationValidityState();
   let [username, setUsername] = useState("");
   let [email, setEmail] = useState("");
   let [password, setPassword] = useState("");
 
+  const EMAIL_REF = useRef<TextInput>(null);
+  const PASSWORD_REF = useRef<TextInput>(null);
+  const REPEAT_PASSWORD_REF = useRef<TextInput>(null);
+
   return (
     <View
       style={[
         {
           flex: 1,
-          justifyContent: "center",
           alignItems: "stretch",
           paddingHorizontal: 28,
+          paddingTop: 27,
+          paddingBottom: 22,
         },
-        TestStyles,
       ]}
     >
       <Heading5>Register</Heading5>
 
-      <UsernameInputComponent
-        isValidState={inputValidity.usernameValidity}
-        onChangeText={(text) => setUsername(text)}
-        onValidation={(isValid) =>
-          updateInputValidity({ usernameValidity: isValid })
-        }
-      />
-
-      <EmailInputComponent
-        isValidState={inputValidity.emailValidity}
-        onChangeText={(text) => setEmail(text)}
-        onValidation={(isValid) =>
-          updateInputValidity({ emailValidity: isValid })
-        }
-      />
-
-      <PasswordInputComponent
-        isValidState={inputValidity.passwordValidity}
-        onChangeText={(text) => setPassword(text)}
-        onValidation={(isValid) =>
-          updateInputValidity({ passwordValidity: isValid })
-        }
-      />
-
-      <RepeatPasswordInputComponent
-        isValidState={inputValidity.repeatPasswordValidity}
-        onEndInput={(repeatedPassword) =>
-          updateInputValidity({
-            repeatPasswordValidity: repeatedPassword === password,
-          })
-        }
-      />
-      <PrimaryButton
-        title={
-          registrationState instanceof RegistrationLoading
-            ? "Loading..."
-            : "Create Account"
-        }
-        disabled={
-          inputValidity.areAnyInputsInvalid() ||
-          registrationState instanceof RegistrationLoading
-        }
-        onPress={function () {
-          // TODO: Pass Input States
-          startRegistration(new RegistrationDetails(username, email, password));
+      <View
+        style={{
+          height: "52.5%",
+          justifyContent: "space-between",
+          paddingTop: 27,
         }}
-      />
+      >
+        <UsernameInputComponent
+          isValidState={inputValidity.usernameValidity}
+          onChangeText={(text) => {
+            setUsername(text);
+            setIsBeingEdited(true);
+          }}
+          onSubmitEditing={() => EMAIL_REF.current?.focus()}
+          onValidation={(isValid) => {
+            updateInputValidity({ usernameValidity: isValid });
+            setIsBeingEdited(false);
+          }}
+        />
+
+        <EmailInputComponent
+          isValidState={inputValidity.emailValidity}
+          onChangeText={(text) => {
+            setEmail(text);
+            setIsBeingEdited(true);
+          }}
+          onSubmitEditing={() => PASSWORD_REF.current?.focus()}
+          onValidation={(isValid) => {
+            updateInputValidity({ emailValidity: isValid });
+            setIsBeingEdited(false);
+          }}
+          ref={EMAIL_REF}
+        />
+
+        <PasswordInputComponent
+          isValidState={inputValidity.passwordValidity}
+          onChangeText={(text) => {
+            setPassword(text);
+            setIsBeingEdited(true);
+          }}
+          onSubmitEditing={() => REPEAT_PASSWORD_REF.current?.focus()}
+          onValidation={(isValid) => {
+            updateInputValidity({ passwordValidity: isValid });
+            setIsBeingEdited(false);
+          }}
+          ref={PASSWORD_REF}
+        />
+
+        <RepeatPasswordInputComponent
+          isValidState={inputValidity.repeatPasswordValidity}
+          onEndInput={(repeatedPassword) =>
+            updateInputValidity({
+              repeatPasswordValidity: repeatedPassword === password,
+            })
+          }
+          ref={REPEAT_PASSWORD_REF}
+        />
+      </View>
 
       {registrationState instanceof RegistrationFailure ||
       registrationState instanceof NetworkFailure ? (
         <Text>{registrationState.message}</Text>
       ) : null}
+
+      <View style={{ paddingTop: 162 }}>
+        <PrimaryButton
+          title={
+            registrationState instanceof RegistrationLoading
+              ? "Loading..."
+              : "Create Account"
+          }
+          disabled={
+            inputValidity.areAnyInputsInvalid() ||
+            registrationState instanceof RegistrationLoading ||
+            isBeingEdited
+          }
+          onPress={function () {
+            //TODO: clear Focus and check validity again
+            startRegistration(
+              new RegistrationDetails(username, email, password),
+            );
+          }}
+        />
+
+        <Text
+          style={{
+            textAlign: "center",
+            paddingTop: 16,
+            color: Colors.blue.dark,
+            ...Fonts.paragraph.p5,
+            lineHeight: 22,
+          }}
+        >
+          By continuing, you agree to our Terms of Service and Privacy Policy.
+        </Text>
+      </View>
     </View>
   );
 }
--- components/InputComponents.tsx
+++ components/InputComponents.tsx
@@ -1,5 +1,8 @@
-import { TestStyles } from "@/app/(tabs)";
+import { Colors } from "@/constants/Colors";
+import { Fonts } from "@/constants/Fonts";
 import { type NullBoolean } from "@/hooks/useRegistrationValidityState";
+import { Image } from "expo-image";
+import { ForwardedRef, forwardRef, useState } from "react";
 import {
   Text,
   TextInput,
@@ -8,42 +11,26 @@ import {
   type NativeSyntheticEvent,
   type ReturnKeyType,
   type TextInputEndEditingEventData,
+  ViewStyle,
+  StyleProp,
+  TextInputSubmitEditingEventData,
 } from "react-native";
 
-const InvalidStyles = { borderColor: "red", borderWidth: 2 };
-
-interface InputComponentProps {
+interface SpecialInputComponentProps {
   isValidState: NullBoolean;
   onChangeText: (text: string) => void;
+  onSubmitEditing?: (
+    event: NativeSyntheticEvent<TextInputSubmitEditingEventData>,
+  ) => void;
   onValidation: (isValid: boolean) => void;
-}
-
-export function EmailInputComponent({
-  isValidState,
-  onChangeText,
-  onValidation,
-}: InputComponentProps) {
-  return (
-    <InputComponent
-      labelText="E-Mail"
-      isValid={isValidState ?? true}
-      hint="e.g. jane-doe.uk@gmail.com"
-      keyboardType="email-address"
-      returnKeyType="next"
-      onChangeText={onChangeText}
-      onEndEditing={function ({ nativeEvent: { text } }) {
-        const REGEX = /^[\w\-\.]{1,63}@[\w-]{1,63}\.[\w]{2,63}$/;
-        onValidation(REGEX.test(text));
-      }}
-    />
-  );
 }
 
 export function UsernameInputComponent({
   isValidState,
   onChangeText,
+  onSubmitEditing,
   onValidation,
-}: InputComponentProps) {
+}: SpecialInputComponentProps) {
   return (
     <InputComponent
       labelText="Username (Nickname)"
@@ -52,91 +39,204 @@ export function UsernameInputComponent({
       keyboardType="default"
       returnKeyType="next"
       onChangeText={onChangeText}
+      onSubmitEditing={onSubmitEditing}
       onEndEditing={function ({ nativeEvent: { text } }) {
-        const REGEX = /^[\p{L}\s]{1,50}$/u;
+        const REGEX = /^\p{L}{1,25}(?:\s\p{L}{1,25})?$/u;
         onValidation(REGEX.test(text));
       }}
+      placeholder="How should we call you?"
     />
   );
 }
 
-export function PasswordInputComponent({
-  isValidState,
-  onChangeText,
-  onValidation,
-}: InputComponentProps) {
+export const EmailInputComponent = forwardRef(function EmailInputComponent(
+  {
+    isValidState,
+    onChangeText,
+    onSubmitEditing,
+    onValidation,
+  }: SpecialInputComponentProps,
+  ref?: ForwardedRef<TextInput>,
+) {
   return (
     <InputComponent
-      labelText="Password"
+      labelText="E-Mail"
       isValid={isValidState ?? true}
-      hint="must contain at least 8 characters, numbers or symbols"
-      keyboardType="default"
+      hint="e.g. jane-doe.uk@gmail.com"
+      keyboardType="email-address"
       returnKeyType="next"
       onChangeText={onChangeText}
+      onSubmitEditing={onSubmitEditing}
       onEndEditing={function ({ nativeEvent: { text } }) {
-        const REGEX = /^[\p{L}\p{P}0-9]{8,100}$/u;
+        const REGEX = /^[\w\-\.]{1,63}@[\w-]{1,63}\.[\w]{2,63}$/;
         onValidation(REGEX.test(text));
       }}
-      isSecureText
+      placeholder="Enter your Email"
+      ref={ref}
     />
   );
-}
+});
 
-export function RepeatPasswordInputComponent({
-  isValidState,
-  onEndInput,
-}: {
-  isValidState: NullBoolean;
-  onEndInput: (repeatedPassword: string) => void;
-}) {
-  return (
-    <InputComponent
-      labelText="Confirm Password"
-      isValid={isValidState ?? true}
-      hint="This must exactly match your selected password"
-      keyboardType="default"
-      returnKeyType="done"
-      onEndEditing={function ({ nativeEvent: { text } }) {
-        onEndInput(text);
-      }}
-      isSecureText
-    />
-  );
-}
+export const PasswordInputComponent = forwardRef(
+  function PasswordInputComponent(
+    {
+      isValidState,
+      onChangeText,
+      onSubmitEditing,
+      onValidation,
+    }: SpecialInputComponentProps,
+    ref?: ForwardedRef<TextInput>,
+  ) {
+    return (
+      <InputComponent
+        labelText="Password"
+        isValid={isValidState ?? true}
+        hint="must contain at least 8 characters, numbers or symbols"
+        keyboardType="default"
+        returnKeyType="next"
+        onChangeText={onChangeText}
+        onSubmitEditing={onSubmitEditing}
+        onEndEditing={function ({ nativeEvent: { text } }) {
+          const REGEX = /^[\p{L}\p{P}0-9]{8,100}$/u;
+          onValidation(REGEX.test(text));
+        }}
+        isSecureText
+        ref={ref}
+      />
+    );
+  },
+);
 
-export function InputComponent({
-  labelText,
-  isValid,
-  keyboardType,
-  returnKeyType = "done",
-  hint,
-  onChangeText,
-  onEndEditing,
-  isSecureText = false,
-}: {
+export const RepeatPasswordInputComponent = forwardRef(
+  function RepeatPasswordInputComponent(
+    {
+      isValidState,
+      onEndInput,
+    }: {
+      isValidState: NullBoolean;
+      onEndInput: (repeatedPassword: string) => void;
+    },
+    ref?: ForwardedRef<TextInput>,
+  ) {
+    return (
+      <InputComponent
+        labelText="Confirm Password"
+        isValid={isValidState ?? true}
+        hint="This must exactly match your selected password"
+        keyboardType="default"
+        returnKeyType="done"
+        onEndEditing={function ({ nativeEvent: { text } }) {
+          onEndInput(text);
+        }}
+        isSecureText
+        ref={ref}
+      />
+    );
+  },
+);
+
+interface BaseInputComponentProps {
   labelText: string;
   isValid: boolean;
   hint: string;
   keyboardType: KeyboardTypeOptions;
   returnKeyType: ReturnKeyType;
   onChangeText?: (text: string) => void;
+  onSubmitEditing?: (
+    event: NativeSyntheticEvent<TextInputSubmitEditingEventData>,
+  ) => void;
   onEndEditing: (
     event: NativeSyntheticEvent<TextInputEndEditingEventData>,
   ) => void;
   isSecureText?: boolean;
-}) {
-  return (
-    <View>
-      <Text>{labelText}</Text>
-      <TextInput
-        keyboardType={keyboardType}
-        returnKeyType={returnKeyType}
-        secureTextEntry={isSecureText}
-        style={[TestStyles, isValid ? {} : InvalidStyles]}
-        onChangeText={onChangeText}
-        onEndEditing={onEndEditing}
-      />
-      <Text style={isValid ? {} : { color: "red" }}>{hint}</Text>
-    </View>
-  );
+  placeholder?: string;
+  style?: StyleProp<ViewStyle>;
+}
+
+export const InputComponent = forwardRef(
+  //TODO: USE ...props
+  function InputComponent(
+    {
+      labelText,
+      isValid,
+      keyboardType,
+      returnKeyType = "done",
+      hint,
+      onChangeText,
+      onSubmitEditing,
+      onEndEditing,
+      isSecureText = false,
+      placeholder,
+      style,
+    }: BaseInputComponentProps,
+    ref?: ForwardedRef<TextInput>,
+  ) {
+    let [isEmpty, setIsEmpty] = useState(true);
+
+    return (
+      <View style={style}>
+        <Text
+          style={[
+            { color: Colors.grey.dark3, paddingBottom: 4 },
+            Fonts.paragraph.p9,
+          ]}
+        >
+          {labelText}
+        </Text>
+        <View style={{ flexDirection: "row" }}>
+          <TextInput
+            keyboardType={keyboardType}
+            returnKeyType={returnKeyType}
+            secureTextEntry={isSecureText}
+            placeholder={placeholder}
+            placeholderTextColor={Colors.grey.dark2}
+            style={[
+              { color: Colors.blue.grey },
+              { flex: 6 },
+              {
+                borderColor: determineBorderColor(isValid, isEmpty),
+                borderRadius: 8,
+                borderWidth: 1,
+                alignItems: "stretch",
+                paddingHorizontal: 14,
+                paddingVertical: 6.25,
+                ...Fonts.paragraph.p4,
+              },
+            ]}
+            onChangeText={onChangeText}
+            onSubmitEditing={onSubmitEditing}
+            blurOnSubmit={onSubmitEditing ? false : true}
+            onEndEditing={(event) => {
+              onEndEditing(event);
+              setIsEmpty(event.nativeEvent.text.length === 0 ? true : false);
+            }}
+            ref={ref}
+          />
+          <Image
+            source={require("@/assets/images/EyeHidden.svg")}
+            contentFit="fill"
+            style={{ height: 20, aspectRatio: 1, padding: 12 }}
+          />
+        </View>
+        <Text
+          style={{
+            color: isValid ? Colors.grey.dark3 : "red",
+            ...Fonts.paragraph.p8,
+          }}
+        >
+          {hint}
+        </Text>
+      </View>
+    );
+  },
+);
+
+function determineBorderColor(isValid: boolean, isEmpty: boolean): string {
+  if (!isValid) {
+    return Colors.red;
+  } else if (isEmpty) {
+    return Colors.grey.dark1;
+  } else {
+    return Colors.grey.dark2;
+  }
 }
