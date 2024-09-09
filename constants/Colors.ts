fe22e8c7c0fc7a392c48984f811f7d3c047fdb7d
Christian Sturm <c.sturm@avm.de>
2024-09-09 18:03:38 +0200

    Feat: Add more Colots and Fonts

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,10 +1,16 @@
 export const Colors = {
   grey: {
+    light1: "#F3F4F5",
+    light2: "#F0F1F3",
+    light3: "#E5E7E9",
     dark1: "#D0D5DD",
+    dark2: "#9EA8B3",
+    dark3: "#737C8D",
   },
   blue: {
     dark: "#1D3D5E",
     grey: "#142132",
   },
   white: "#FFFFFF",
+  red: "#D12E34",
 };
--- constants/Fonts.ts
+++ constants/Fonts.ts
@@ -6,8 +6,34 @@ const h5: StyleProp<TextStyle> = {
   lineHeight: 28,
 };
 
+const p4: StyleProp<TextStyle> = {
+  fontSize: 16,
+  fontWeight: "regular",
+};
+
+const p5: StyleProp<TextStyle> = {
+  fontSize: 14,
+  fontWeight: "regular",
+};
+
+const p8: StyleProp<TextStyle> = {
+  fontSize: 12,
+  fontWeight: "regular",
+};
+
+const p9: StyleProp<TextStyle> = {
+  fontSize: 12,
+  fontWeight: 500,
+};
+
 export const Fonts = {
   title: {
     h5,
+  },
+  paragraph: {
+    p9,
+    p8,
+    p5,
+    p4,
   },
 };
