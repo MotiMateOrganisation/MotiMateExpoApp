3865c0e5c4ecbb9cd896b6ffeedebcd6969f403b
Christian Sturm <c.sturm@avm.de>
2024-10-04 14:11:33 +0200

    Feat: Finalize Screen and behaviour

--- app/initial-group/create.tsx
+++ app/initial-group/create.tsx
@@ -4,8 +4,15 @@ import { Fonts } from "@/constants/Fonts";
 import { Colors } from "@/constants/Colors";
 import useAndroidBackButtonInputHandling from "@/hooks/useAndroidBackButtonInputHandling";
 import { View, Text, StyleSheet, TextInput } from "react-native";
-import { useState } from "react";
+import { PropsWithChildren, useState } from "react";
 import { isEmpty } from "@/utils/StringHelpers";
+import useGroupCreationState from "@/hooks/group/useGroupCreationState";
+import {
+  GroupCreationSuccess,
+  NetworkError,
+  RequestError,
+  RequestStatus,
+} from "@/utils/RegistrationStatus";
 
 const GROUP_NAME_PATTERN = /^[\p{L}\p{So}]+(?:\s[\p{L}\p{So}]+){0,4}$/u;
 
@@ -19,18 +26,14 @@ const styles = StyleSheet.create({
     color: Colors.grey.dark3,
     ...Fonts.paragraph.p9,
   },
-  inputText: {
-    color: Colors.blue.grey,
-    ...Fonts.paragraph.p4,
-  },
 });
 
 export default function GroupCreationScreen() {
   useAndroidBackButtonInputHandling();
 
   const [groupName, setGroupName] = useState("");
-  const [isValid, setIsValid] = useState(false);
-  const [groupNameState, setGroupNameState] = useGroupNameState();
+  const [isValid, setIsValid] = useState(true);
+  const [groupCreationState, startGroupCreation] = useGroupCreationState();
 
   return (
     <View
@@ -49,19 +52,18 @@ export default function GroupCreationScreen() {
         <Text style={styles.topText}>Enter a name for your group chat</Text>
       </View>
 
-      <View style={{ height: "10%" }} />
       <View
         style={{
-          alignSelf: "center",
-          height: "24%",
+          height: "12%",
+          alignItems: "stretch",
           justifyContent: "space-between",
         }}
       >
-        <Text style={[styles.middleText, { textAlign: "center" }]}>
-          Group Name
-        </Text>
+        <Text style={[styles.middleText]}>Group Name</Text>
         <TextInput
           maxLength={30}
+          placeholder="Enter a Group Name"
+          placeholderTextColor={Colors.grey.dark2}
           onChange={function handleInput({ nativeEvent: { text } }) {
             setGroupName(text);
           }}
@@ -70,29 +72,33 @@ export default function GroupCreationScreen() {
           }}
           value={groupName}
           style={[
-            styles.inputText,
+            BASE_INPUT_STYLES.basic,
             {
-              color: determineColor(),
               borderColor: determineBorderColor(),
-              borderWidth: 1,
             },
           ]}
         />
       </View>
 
-      <View style={{ height: "40%", justifyContent: "center" }}></View>
+      <View style={{ height: "40%", justifyContent: "center" }}>
+        {determineErrorComponent()}
+      </View>
 
       <View
         style={{
           height: "14%",
-          justifyContent: "space-between",
+          justifyContent: "flex-end",
         }}
       >
         <PrimaryButton
-          title={"Next Step"}
+          title={
+            groupCreationState instanceof GroupCreationSuccess
+              ? groupCreationState.joinCode
+              : "Next Step"
+          }
           disabled={isEmpty(groupName) || !isValid}
           onPress={() => {
-            setGroupNameState(groupName);
+            startGroupCreation(groupName);
           }}
         />
       </View>
@@ -101,7 +107,7 @@ export default function GroupCreationScreen() {
 
   function determineBorderColor() {
     if (isEmpty(groupName)) {
-      return Colors.grey.dark1;
+      return Colors.grey.dark2;
     } else if (!isValid) {
       return Colors.red;
     } else {
@@ -109,11 +115,49 @@ export default function GroupCreationScreen() {
     }
   }
 
-  function determineColor() {
-    if (isEmpty(groupName)) {
-      return Colors.grey.dark1;
+  function determineErrorComponent() {
+    if (!isValid) {
+      return (
+        <ErrorText>
+          {
+            "Please only provide Letters and Emojis seperated by up to a single Space for your Group's name"
+          }
+        </ErrorText>
+      );
+    } else if (isErrorState(groupCreationState)) {
+      return <ErrorText>{groupCreationState.message}</ErrorText>;
     } else {
-      return Colors.blue.grey;
+      return null;
     }
   }
+
+  //TODO: Refactor out
+  function isErrorState(
+    state: RequestStatus | null,
+  ): state is RequestError | NetworkError {
+    return (
+      groupCreationState instanceof RequestError ||
+      groupCreationState instanceof NetworkError
+    );
+  }
+
+  //TODO: Refactor out
+  function ErrorText(props: PropsWithChildren) {
+    return (
+      <Text style={[styles.topText, { color: Colors.red }]}>
+        {props.children}
+      </Text>
+    );
+  }
 }
+
+export const BASE_INPUT_STYLES = StyleSheet.create({
+  basic: {
+    color: Colors.blue.grey,
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 14,
+    paddingVertical: 10,
+    ...Fonts.paragraph.p4,
+  },
+});
