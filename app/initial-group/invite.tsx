4e1602b7187a991cbe2bdab6e0e6431fdb95317e
Christian Sturm <c.sturm@avm.de>
2024-10-04 17:38:41 +0200

    Feat: Add Clipboard and finalize screen

--- app/initial-group/invite.tsx
+++ app/initial-group/invite.tsx
@@ -2,12 +2,13 @@ import { PrimaryButton } from "@/components/Buttons";
 import { Heading5 } from "@/components/Headings";
 import { Fonts } from "@/constants/Fonts";
 import { Colors } from "@/constants/Colors";
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import { View, Text, StyleSheet, TextInput, Pressable } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import { Image } from "expo-image";
+import { setStringAsync } from "expo-clipboard";
 
 export default function InviteScreen() {
-  const { joinCode } = useLocalSearchParams<{ joinCode?: string }>();
+  const { joinCode } = useLocalSearchParams<{ joinCode: string }>();
   return (
     <>
       <View
@@ -23,16 +24,46 @@ export default function InviteScreen() {
       >
         <View>
           <Heading5>Invite Friends!</Heading5>
-          <Text style={[styles.topText, { width: "90%", alignSelf: "center" }]}>
+          <Text
+            style={[TEXT_STYLES.topText, { width: "90%", alignSelf: "center" }]}
+          >
             Share this referral code to invite your friends to the group chat
           </Text>
         </View>
 
-        <TextInput
-          readOnly={true}
-          value={joinCode}
-          style={[BASE_INPUT_STYLES.copy, { height: "12%" }]}
-        />
+        <View
+          style={[
+            {
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+              height: "12%",
+            },
+            BASE_INPUT_STYLES.copyField,
+          ]}
+        >
+          <TextInput
+            readOnly={true}
+            value={joinCode}
+            style={[TEXT_STYLES.copyText, { paddingTop: 30 }]}
+          />
+          <Pressable
+            onPress={async function copyToClipboard() {
+              await setStringAsync(joinCode);
+            }}
+            style={[
+              BASE_BUTTON_STYLES.secondary,
+              {
+                justifyContent: "center",
+                alignItems: "center",
+                height: "80%",
+                aspectRatio: 1.25,
+              },
+            ]}
+          >
+            <Text style={TEXT_STYLES.secondaryButtonText}>Copy</Text>
+          </Pressable>
+        </View>
 
         <View style={{ height: "50%" }} />
 
@@ -53,7 +84,7 @@ export default function InviteScreen() {
   );
 }
 
-const styles = StyleSheet.create({
+const TEXT_STYLES = StyleSheet.create({
   topText: {
     color: Colors.grey.dark3,
     ...Fonts.paragraph.p5,
@@ -63,15 +94,22 @@ const styles = StyleSheet.create({
     color: Colors.grey.dark3,
     ...Fonts.paragraph.p9,
   },
+  secondaryButtonText: { color: Colors.blue.grey, ...Fonts.paragraph.p1 },
+  copyText: { color: Colors.white, ...Fonts.title.h2 },
 });
 
 export const BASE_INPUT_STYLES = StyleSheet.create({
-  copy: {
-    color: Colors.white,
+  copyField: {
     backgroundColor: Colors.blue.grey,
     borderRadius: 100,
-    paddingHorizontal: 24,
-    paddingTop: 28,
-    ...Fonts.title.h2,
+    paddingStart: 24,
+    paddingEnd: 6,
+  },
+});
+
+export const BASE_BUTTON_STYLES = StyleSheet.create({
+  secondary: {
+    backgroundColor: Colors.white,
+    borderRadius: 100,
   },
 });
