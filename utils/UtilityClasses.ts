5acf98e99f371e31b8f448e40d7740c9b7628178
Christian Sturm <c.sturm@avm.de>
2024-10-12 22:31:10 +0200

    feat: update type

--- utils/UtilityClasses.ts
+++ utils/UtilityClasses.ts
@@ -1,6 +1,6 @@
 import { FormatError } from "./CustomErrors";
 
-export type SafeDigits = string;
+export type SafeDigits = `${number}`;
 
 /**
  * @throws {FormatError} on {@link safeDigits} having non numeric characters
@@ -10,7 +10,7 @@ export class DigitString {
 
   constructor(unsafeDigits: string) {
     if (/^\d+$/.test(unsafeDigits)) {
-      this.safeDigits = unsafeDigits;
+      this.safeDigits = unsafeDigits as SafeDigits;
     } else {
       throw new FormatError(
         `The supplied string "${unsafeDigits}" contains non numeric digits!`,
