5afb3e845110a7bc75cce9e7ae365f68d27338e2
Christian Sturm <c.sturm@avm.de>
2024-10-14 16:17:45 +0200

    refac: extract body styles

---
+++ constants/styles/Body.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export const BODY_STYLES = StyleSheet.create({
+  nonScrollable: {
+    alignSelf: "center",
+    justifyContent: "space-between",
+    alignItems: "stretch",
+    width: "85%",
+    height: "90%",
+  },
+});
