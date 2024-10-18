2206a67aba20b2160f47ffe7911a461de312961d
Christian Sturm <c.sturm@avm.de>
2024-10-18 12:20:38 +0200

    feat: add first new tabbar implementation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,23 +1,48 @@
-import FontAwesome from "@expo/vector-icons/MaterialIcons";
+import { Colors } from "@/constants/Colors";
+import { Fonts } from "@/constants/Fonts";
+import { Icon } from "@/constants/Icons";
 import { Tabs } from "expo-router";
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false, tabBarActiveTintColor: "blue" }}>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveBackgroundColor: Colors.grey.light1,
+        tabBarStyle: { height: "20%" },
+        tabBarActiveTintColor: Colors.blue.grey,
+        tabBarInactiveTintColor: Colors.grey.dark2,
+        tabBarIconStyle: { fontSize: 28 },
+        tabBarLabelStyle: Fonts.paragraph.p10,
+        // tabBarInactiveBackgroundColor: Colors.
+        // tabBarBackground: () => null,
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="space-dashboard" color={color} />
+          tabBarIcon: ({ color, size }) => (
+            <Icon tintColor={color} size={size} icon="Dashboard" />
           ),
+          tabBarLabel: "Dashboard",
         }}
       />
       <Tabs.Screen
         name="group"
         options={{
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="chat-bubble" color={color} />
+          tabBarIcon: ({ color, size }) => (
+            <Icon tintColor={color} size={size} icon="Chat" />
           ),
+          tabBarLabel: "My Group",
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon tintColor={color} size={size} icon="Profile" />
+          ),
+          tabBarLabel: "My Profile",
         }}
       />
     </Tabs>
