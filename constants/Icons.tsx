1f0c295264813c42bfbe5aefbf49a50115b59907
Christian Sturm <c.sturm@avm.de>
2024-10-18 12:20:13 +0200

    feat: add Icon component

---
+++ constants/Icons.tsx
@@ -0,0 +1,21 @@
+import { Image } from "expo-image";
+
+export function Icon(props: {
+  tintColor: string;
+  size: number;
+  icon: keyof typeof Icons;
+}) {
+  const { tintColor, icon } = props;
+  return (
+    <Image
+      style={{ tintColor, width: props.size, aspectRatio: 1 }}
+      source={Icons[icon]}
+    />
+  );
+}
+
+export const Icons = {
+  Dashboard: require("@/assets/images/optimized_svg/DashboardIcon.svg"),
+  Chat: require("@/assets/images/optimized_svg/ChatIcon.svg"),
+  Profile: require("@/assets/images/optimized_svg/ProfileIcon.svg"),
+};
